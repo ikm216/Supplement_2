95db84fde0175068b1cea8899ff572907ef0cda6
ikm216 <ikmclassof22@gmail.com>
2024-11-21 19:22:11 -0500

    feat: implemented area_cir

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ def area_rect(len, wid):
     """
     return len * wid
 
+def area_cir(base, hei):
+    return (base * hei) / 2
+
 def test_should_return_24_for_6_and_4():
     assert area_rect(6, 4) == 24
 
