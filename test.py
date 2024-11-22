6032237a521b75b049682a412148da85f93e30f4
ikm216 <ikmclassof22@gmail.com>
2024-11-21 19:26:26 -0500

    test: added test for area of triangle

--- test.py
+++ test.py
@@ -11,10 +11,19 @@ def area_rect(len, wid):
     return len * wid
 
 def area_cir(base, hei):
-    return (base * hei) / 2
+     """ Use the area of a circle formula and returns the area
+
+    Args:
+        base: the base of the 
+        hei: the width of the rectangle
+
+    Returns:
+        The area of the rectangle
+    """
+     return (base * hei) / 2
 
 def test_should_return_24_for_6_and_4():
     assert area_rect(6, 4) == 24
 
 def test_should_return_12_for_6_and_4():
-    assert area_cir(6, 4) == 12
+    assert area_tri(6, 4) == 12
